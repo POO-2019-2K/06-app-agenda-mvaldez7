777874a11dea43260095578b0d4afe6da43e28bb
MARTIN IGNACIO VALDEZ MARMOLEJO <42812035+mvaldez7@users.noreply.github.com>
2019-05-28 12:57:27 -0500

    changes part II

---
+++ almost done part ll/Contactos.js
@@ -0,0 +1,36 @@
+export default class Contactos {
+    constructor(Contacto) {
+        this._name =  Contacto.name.toUpperCase();
+        this._cel = Contacto.cel;
+        this._birthday = Contacto.birthday;
+        this._correo = Contacto.correo;
+    }
+//get data//
+    get name(){
+        return this._name;
+    }
+
+    get cel(){
+        return this._cel;
+    }
+    
+    get correo(){
+        return this._correo;
+    }
+
+    getBirthdayAsString(){
+        let d = this._birthday.getDate()+ "/" + this._birthday.getMonth() + "/" + this._birthday.getFullYear();
+        return d;
+    }
+
+    get birthday(){
+        return this._birthday;
+    }
+
+    getAge() {
+        let oneDay= (24*60*60*1000);
+        let oneYear = oneDay * 365;
+        let differenceMs = Math.abs(new Date() - this._birthday);
+        return Math.round(differenceMs / oneYear);
+        }
+}
---
+++ almost done part ll/Tabla.js
@@ -0,0 +1,94 @@
+import Contactos from "./Contactos.js";
+
+export default class Tabla {
+    constructor(tableAgenda){
+        this._tableAgenda = tableAgenda;
+        this._taller2 = [];
+        this._initTables2();
+    }
+
+    _initTables2() {
+        //localStorage.removeItem("taller2");
+        let taller2 = JSON.parse(localStorage.getItem("taller2"));
+        if(!taller2) {
+        return;
+        }
+        taller2.forEach( (Contacto, index) => {
+        console.log(Contacto);
+        Contacto.birthday = new Date(Contacto.birthday);
+        this._addContacto(new Contactos(Contacto));
+        });
+    }
+
+    _addEditDeleteToRow(row, Contacto) {
+        let btnDelete = document.createElement("input");
+        btnDelete.type = "button";
+        btnDelete.value = 'Eliminar';
+        btnDelete.className = 'btn btn-danger';
+    
+        row.cells[5].innerHTML = '';
+        row.cells[5].appendChild(btnDelete);
+        btnDelete.addEventListener('click', () => {
+            this._taller2.splice(Contacto, 1);
+            row.innerHTML = "";
+            localStorage.setItem("taller2", JSON.stringify(this._taller2));
+            return;
+        });
+    }
+    
+    _addContacto(Contacto) {
+        let row = this._tableAgenda.insertRow(-1);
+        let cellName = row.insertCell(0);
+        let cellCel = row.insertCell(1);
+        let cellBirthday = row.insertCell(2);
+        let cellAge = row.insertCell(3);
+        let cellCorreo = row.insertCell(4);
+        row.insertCell(5);
+
+
+        cellName.innerHTML = Contacto.name;
+        cellCel.innerHTML = Contacto.cel;
+        cellBirthday.innerHTML = Contacto.getBirthdayAsString();
+        cellAge.innerHTML = Contacto.getAge();
+        cellCorreo.innerHTML = Contacto.correo;
+        this._addEditDeleteToRow(row, Contacto);
+
+        let objContacto = {
+            name: Contacto.name,
+            cel: Contacto.cel,
+            birthday: Contacto.birthday,
+            correo: Contacto.correo,
+        }
+            this._taller2.push(objContacto);
+    }
+    
+_findCorreo(correo){
+    let found = -1
+
+    this._taller2.forEach((Contacto, index)=>{
+        if(Contacto.correo === correo){
+            found = index;
+            return;
+        }
+    });
+    return found;
+}
+
+
+
+addEmployee2(Contacto) {
+    let found = this._findCorreo(Contacto.correo);
+    if (found >= 0){
+        swal.fire({
+            type: "error",
+            tittle: "error",
+            text: "esta persona ya esta registrada"
+        });
+        return;
+    }
+    this._addContacto(Contacto);
+    localStorage.setItem("taller2", JSON.stringify(this._taller2));
+    console.log(localStorage.getItem("taller2"));
+}
+
+}
